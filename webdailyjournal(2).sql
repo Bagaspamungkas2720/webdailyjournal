40c11e9e5b241812059b4edfe54fa3d6a0ecbda9
Bagaspamungkas2720 <142129512+Bagaspamungkas2720@users.noreply.github.com>
2025-01-07 20:19:02 +0700

    Add files via upload

---
+++ webdailyjournal(2).sql
@@ -0,0 +1,140 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 07 Jan 2025 pada 13.48
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `webdailyjournal`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `article`
+--
+
+CREATE TABLE `article` (
+  `id` int(11) NOT NULL,
+  `judul` text NOT NULL,
+  `isi` text NOT NULL,
+  `gambar` text NOT NULL,
+  `tanggal` datetime NOT NULL,
+  `username` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `article`
+--
+
+INSERT INTO `article` (`id`, `judul`, `isi`, `gambar`, `tanggal`, `username`) VALUES
+(5, 'BECIKU', 'Dosen dan tim mahasiswa Universitas Dian\r\n              Nuswantoro (Udinus) Semarang baru-baru ini berinovasi membuat\r\n              Becik-KU (Becak Listrik Kampus Universitas Dian Nuswantoro). Tim\r\n              terdiri dari 6 mahasiswa dan 6 dosen Udinus.\r\n     ', '20241230203626.jpeg', '2024-12-30 20:36:26', 'admin'),
+(6, 'catur', 'robot catur udinus', '20250107160347.jpeg', '2025-01-07 16:03:47', 'admin'),
+(7, 'robot gamelan', 'robot gamelan udinus', '20250107160428.jpeg', '2025-01-07 16:04:28', 'admin'),
+(8, 'siadin', 'siadin udinus', '20250107160627.jpg', '2025-01-07 16:06:27', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `gallery`
+--
+
+CREATE TABLE `gallery` (
+  `id` int(11) NOT NULL,
+  `nama_gambar` varchar(255) NOT NULL,
+  `judul_gambar` varchar(255) DEFAULT NULL,
+  `deskripsi` text DEFAULT NULL,
+  `tanggal_upload` date DEFAULT NULL,
+  `kategori` varchar(50) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `gallery`
+--
+
+INSERT INTO `gallery` (`id`, `nama_gambar`, `judul_gambar`, `deskripsi`, `tanggal_upload`, `kategori`, `user_id`) VALUES
+(1, '67736e6b1138e.jpeg', 'beciku', 'becak listrik udin', NULL, NULL, NULL),
+(3, '67736eb039970.jpeg', 'catur', 'catur', NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` text NOT NULL,
+  `foto` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `foto`) VALUES
+(1, 'admin', 'e10adc3949ba59abbe56e057f20f883e', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `article`
+--
+ALTER TABLE `article`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `gallery`
+--
+ALTER TABLE `gallery`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `article`
+--
+ALTER TABLE `article`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT untuk tabel `gallery`
+--
+ALTER TABLE `gallery`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
